040a36e79f8f0efc34e07d97c275ef3c38a5e4d2
jukka.hyvarinen <jukka.hyvarinen@innofactor.com>
2018-07-25 12:24:19 +0300

    add proper tests

---
+++ JsonComplexTypeTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace EfJsonTest {
+
+    [TestClass]
+    public class JsonComplexTypeTests {
+
+        public class Address {
+
+            public string Street { get; set; }
+
+            public bool Equals(Address obj) {
+                return Street == obj.Street;
+            }
+
+            public override int GetHashCode() {
+                return Street.GetHashCode();
+            }
+
+            public Address Clone() {
+                return new Address { Street = Street };
+            }
+
+        }
+
+        public class Customer {
+            public int Id { get; set; }
+            public Address Address { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class CustomerDbContext : DbContext {
+
+            public DbSet<Customer> Customers { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+                base.OnConfiguring(optionsBuilder);
+                optionsBuilder.UseInMemoryDatabase("Test");
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder) {
+             
+                modelBuilder.Entity<Customer>().Property(m => m.Address).Metadata.SetValueComparer(new ValueComparer<Address>(
+                    (p1, p2) => p1.Equals(p2),
+                    p => p != null ? p.GetHashCode() : 0,
+                    p => p != null ? p.Clone() : default));
+                modelBuilder.Entity<Customer>().Property(m => m.Address)
+                    .HasConversion(
+                        v => v != null ? JsonConvert.SerializeObject(v) : null,
+                        v => v != null ? JsonConvert.DeserializeObject<Address>(v) : default);
+                
+            }
+
+        }
+
+        private readonly CustomerDbContext db = new CustomerDbContext();
+
+        private void AssertModified(Customer customer) {
+            db.ChangeTracker.DetectChanges();
+            Assert.AreEqual(EntityState.Modified, db.Entry(customer).State, "Entity state");
+        }
+
+        private void AssertAddressModified(Customer customer) {
+            Assert.IsTrue(db.Entry(customer).Property(m => m.Address).IsModified, "Address is modified");
+        }
+
+        [TestMethod]
+        public void PlainField() {
+
+            var customer = new Customer { Name = "Customer" };
+            db.Add(customer);
+            db.SaveChanges();
+
+            customer.Name = "Updated";
+            AssertModified(customer);
+
+        }
+
+        [TestMethod]
+        public void ComplexField_WithoutReload() {
+
+            var customer = new Customer { Address = new Address { Street = "Street" } };
+            db.Add(customer);
+            db.SaveChanges();
+
+            customer.Address.Street = "Updated";
+            AssertModified(customer);
+            AssertAddressModified(customer);
+
+        }
+        
+        [TestMethod]
+        public void ComplexField_WithReload() {
+
+            var customer = new Customer { Address = new Address { Street = "Street" } };
+            db.Add(customer);
+            db.SaveChanges();
+
+            db.Entry(customer).State = EntityState.Detached;
+            customer = db.Customers.Find(customer.Id);
+
+            customer.Address.Street = "Updated";
+            AssertModified(customer);
+            AssertAddressModified(customer);
+
+        }
+
+        [TestMethod]
+        public void ComplexField_WithAssign() {
+
+            var customer = new Customer { Address = new Address { Street = "Street" } };
+            db.Add(customer);
+            db.SaveChanges();
+
+            customer.Address = new Address { Street = "Updated" };
+
+            AssertModified(customer);
+            AssertAddressModified(customer);
+
+        }
+
+    }
+
+}
